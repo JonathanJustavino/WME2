7ceac04c276f5b58b4f27702ed1aec3706cc4afc
JonathanJustavino <s3190208@mail.zih.tu-dresden.de>
2016-11-09 16:58:03 +0100

    Maintain the indent of file save.php

--- save.php
+++ save.php
@@ -1,20 +1,20 @@
 <?php
-require 'world_data_parser.php';
+  require 'world_data_parser.php';
 
-$myParser = new WorldDataParser;
-$path = './world_data.csv';
-$data =  $myParser->parseCSV($path);
+  $myParser = new WorldDataParser;
+  $path = './world_data.csv';
+  $data =  $myParser->parseCSV($path);
 
-$xmlData = $myParser->saveXML($data);
+  $xmlData = $myParser->saveXML($data);
 
-$parser = xml_parser_create();
-$result = xml_parse($parser, $xmlData);
+  $parser = xml_parser_create();
+  $result = xml_parse($parser, $xmlData);
 
-if($result === 1){
-  echo "XML Savestatus: erfolgreich (1)";
-  return true;
-}
-echo "XML Savestatus: fehlgeschlagen (0)";
-return false;
+  if($result === 1){
+    echo "XML Savestatus: erfolgreich (1)";
+    return true;
+  }
+  echo "XML Savestatus: fehlgeschlagen (0)";
+  return false;
 
  ?>
