96b80f8ac24b2b5fba304043101f1659f6648cec
JonathanJustavino <s3190208@mail.zih.tu-dresden.de>
2016-11-09 15:48:45 +0100

    Rework the function saveXML now trims all arrays except for the first

--- world_data_parser.php
+++ world_data_parser.php
@@ -13,12 +13,23 @@
 
     }
 
+    /**
+    * Use array_shift() to take first elem
+    * array_unshift to put it back
+    *
+    */
+
     public function saveXML($array)
     {
       $simpleXML = new SimpleXMLElement('<data></data>');
+      $first = array_shift($array);
+      foreach ($first as $key => $value) {
+          $simpleXML->addChild("$value");
+      }
       foreach ($array as $key => $value) {
         foreach ($value as $valkey => $valprop) {
-          $simpleXML->addChild("$valprop");
+          $trimmed = trim($valprop);
+          $simpleXML->addChild("$trimmed");
         }
       }
       echo $simpleXML->asXML();
